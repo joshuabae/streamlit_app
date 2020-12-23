55e2dbbe4c8c7f854bd4b9c31c91e2be98f4af92
joshuabae <jsb9@rice.edu>
2020-12-23 23:52:41 +0900

    edits

--- first_app.py
+++ first_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import time
 
 st.title('My first app')
 
@@ -9,11 +10,11 @@ st.title('My first app')
 Here's our first attempt at using data to create a table:
 """
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-df
+# df = pd.DataFrame({
+#   'first column': [1, 2, 3, 4],
+#   'second column': [10, 20, 30, 40]
+# })
+# df
 
 if st.checkbox('Show dataframe'):
     chart_data = pd.DataFrame(
@@ -28,8 +29,31 @@ if st.checkbox('Show dataframe'):
 #
 # st.map(map_data)
 
-option = st.sidebar.selectbox(
-    'Which number do you like best?',
-     df['first column'])
+# option = st.sidebar.selectbox(
+#     'Which number do you like best?',
+#      df['first column'])
+#
+# 'You selected:', option
 
-'You selected:', option
+# left_column, right_column = st.beta_columns(2)
+# pressed = left_column.button('Press me?')
+# if pressed:
+#     right_column.write("Woohoo!")
+#
+# expander = st.beta_expander("FAQ")
+# expander.write("Here you could put in some really, really long explanations...")
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
+
